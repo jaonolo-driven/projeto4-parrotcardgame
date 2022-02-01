a550dc7e7004db08db54ac530e1c83f66a60a8ce
Joao Vitor Cezario Sanches <joaovitormanolo@gmail.com>
2022-02-01 04:51:19 -0300

    Implemented 'Start game' mechanic

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,3 +1,18 @@
 const flipCard = (card) => {
     card.classList.toggle('flipped')
 }
+
+const gameInitialize = () => {
+    if(currentPlaying !== false)
+        return 1
+
+    const gameContainer = document.querySelector('section')
+    const cardCount = prompt('Numero')
+
+    for(let i = 0; i < cardCount; i++){
+        let card =  document.createElement('div')
+        card.classList.add('card')
+        card.addEventListener('click', (e) => flipCard(e.currentTarget))
+        gameContainer.appendChild(card)
+    }
+}
