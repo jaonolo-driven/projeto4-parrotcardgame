cc77e6e9f2e52b7d0b205a8fed5d2970cd83e29e
Joao Vitor Cezario Sanches <joaovitormanolo@gmail.com>
2022-02-01 08:40:21 -0300

    Added round count and changed flipCard from toggle to add

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,18 +1,31 @@
 const flipCard = (card) => {
-    card.classList.toggle('flipped')
+    if(card.classList.contains('flipped'))
+        return 0
+    card.classList.add('flipped')
+
+    roundCount++
+    document.querySelector('p').innerText = roundCount
+    
+    // if(flipped)
+    //     checkFlipped()
+    // flipped
 }
 
 const gameInitialize = () => {
     if(currentPlaying !== false)
         return 1
+    currentPlaying = true
 
     const gameContainer = document.querySelector('section')
     const cardCount = prompt('Numero')
-
+    
     for(let i = 0; i < cardCount; i++){
         let card =  document.createElement('div')
         card.classList.add('card')
         card.addEventListener('click', (e) => flipCard(e.currentTarget))
         gameContainer.appendChild(card)
     }
+    
+    roundCount = 0
+    document.querySelector('p').innerText = roundCount
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,3 @@
 let currentPlaying = false
+let roundCount = 0
+let flipped = 0
